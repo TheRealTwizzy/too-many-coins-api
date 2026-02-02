ea01adfed2e807518fb0bd7b0204690c87fb8d2f
TheRealTwizzy <trenton.streator@gmail.com>
2026-02-01 21:19:52 -0700

    chore: add PostgreSQL connection and health check

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("failed to open database:", err)
+	}
+
+	db.SetMaxOpenConns(5)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(30 * time.Minute)
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("failed to ping database:", err)
+	}
+
+	log.Println("Connected to PostgreSQL")
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +40,7 @@ func main() {
 	})
 
 	log.Println("Server starting on :8080")
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
